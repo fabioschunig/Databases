a1b0e59b048fb8186f7e7b9ab80f728981e1d7b9
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-09 21:26:05 -0300

    Firebird comments in SQL file

--- Firebird/example-database.sql
+++ Firebird/example-database.sql
@@ -1,3 +1,5 @@
+
+-- create user
 > cd "C:\Program Files (x86)\Firebird\Firebird_2_5\bin>"
 
 > gsec -user SYSDBA -password masterkey
@@ -6,8 +8,9 @@ GSEC> add fabio -pw newpass -fname Fabio
 
 GSEC> quit
 
-> mkdir c:\example-database
 
+-- create database file
+> mkdir c:\example-database
 
 > isql -user fabio -password newpass
 
@@ -18,17 +21,22 @@ SQL> SHOW DATABASE 'c:\example-database\example-database.fdb';
 SQL> quit;
 
 
+-- connect database
 > isql -user fabio -password newpass
 
 SQL> CONNECT 'c:\example-database\example-database.fdb';
 Database:  'c:\example-database\example-database.fdb', User: fabio
 
+
+-- table customer
 SQL> CREATE TABLE customer (
     id int not null primary key,
     name varchar(128) not null,
     created timestamp default current_timestamp not null,
     updated timestamp
 );
+
+SQL> ALTER TABLE customer ADD CONSTRAINT pk_customer PRIMARY
 
 SQL> SHOW TABLE customer;
 
