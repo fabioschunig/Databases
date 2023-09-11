65592bab7e5463e98acc2834df65d28b0473d8cc
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-11 20:22:42 -0300

    Firebird tables customer and product

--- Firebird/example-database.sql
+++ Firebird/example-database.sql
@@ -30,14 +30,29 @@ Database:  'c:\example-database\example-database.fdb', User: fabio
 
 -- table customer
 SQL> CREATE TABLE customer (
-    id int not null primary key,
+    id int not null,
     name varchar(128) not null,
     created timestamp default current_timestamp not null,
     updated timestamp
 );
 
-SQL> ALTER TABLE customer ADD CONSTRAINT pk_customer PRIMARY
+SQL> ALTER TABLE customer ADD CONSTRAINT pk_customer PRIMARY KEY (id);
 
 SQL> SHOW TABLE customer;
+
+
+-- table product
+SQL> CREATE TABLE product (
+    id int not null,
+    description varchar(128) not null,
+    price decimal(12,4) default 0 not null,
+    created timestamp default current_timestamp not null,
+    updated timestamp
+);
+
+SQL> ALTER TABLE product ADD CONSTRAINT pk_product PRIMARY KEY (id);
+
+SQL> SHOW TABLE product;
+
 
 SQL> quit;
